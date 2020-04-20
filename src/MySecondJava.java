1ea36ba9eec5a1ff0744e28e464c672cee4f34b4
mbake94 <mbake94@gmail.com>
2020-04-20 11:58:54 -0700

    third commit

--- src/MySecondJava.java
+++ src/MySecondJava.java
@@ -2,5 +2,6 @@ public class MySecondJava {
     public static void main (String[]args) {
         System.out.println("Hello World");
         System.out.println("Today is April 20th, 2020. I am doing well!");
+        System.out.println("My name is Miles.");
     }
 }
